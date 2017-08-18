34c6ab78c52895171a68520c29e3b15404eef03e
Jacky Tan <jackytan146@gmail.com>
2017-08-17 21:56:14 -0400

    this is a message

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  $(".ham").click(function(){
+    var hammenu = $(".hammenu")
+    if (hammenu.hasClass("active")) {
+      hammenu.removeClass("active");
+    } else {
+      hammenu.addClass("active");
+    }
+    console.log(43);
+  })
